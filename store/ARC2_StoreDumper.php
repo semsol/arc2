effd8cdfc85ade2054b04de82229f03ad489619d
Benjamin Nowack <bnowack@semsol.com>
2010-07-06 15:53:58 +0200

    add: support for a buffer file via store_use_dump_dir, store_dump_dir, and store_dump_suffix (e.g. for monthly dumps) options / tweak: dumps are now created in chunks to avoid DB server timeouts / add: getSafeValue method which fixes some critical character serialization bugs

--- store/ARC2_StoreDumper.php
+++ store/ARC2_StoreDumper.php
@@ -1,11 +1,12 @@
 <?php
-/*
-homepage: http://arc.semsol.org/
-license:  http://arc.semsol.org/license
-
-class:    ARC2 Store Dumper
-author:   Benjamin Nowack
-version:  2008-07-02
+/**
+ * ARC2 Store Dumper
+ *
+ * @author Benjamin Nowack
+ * @license <http://arc.semsol.org/license>
+ * @homepage <http://arc.semsol.org/>
+ * @package ARC2
+ * @version 2010-06-09
 */
 
 ARC2::inc('Class');
@@ -23,44 +24,70 @@ class ARC2_StoreDumper extends ARC2_Class {
   function __init() {
     parent::__init();
     $this->store =& $this->caller;
+    $this->keep_time_limit = $this->v('keep_time_limit', 0, $this->a);
+    $this->limit = 100000;
   }
 
   /*  */
   
   function dumpSPOG() {
-    $con = $this->store->getDBCon();
-    $rs = $this->getRecordset();
-    if ($er = mysql_error($con)) $this->addError($er);
     header('Content-Type: application/sparql-results+xml');
-    //header('Content-Type: text/plain');
-    echo $this->getHeader();
-    if ($rs) {
-  		while ($row = mysql_fetch_array($rs)) {
-        echo $this->getEntry($row);
+    if ($this->v('store_use_dump_dir', 0, $this->a)) {
+      $path = $this->v('store_dump_dir', 'dumps', $this->a);
+      /* default: monthly dumps */
+      $path_suffix = $this->v('store_dump_suffix', date('Y_m'), $this->a);
+      $path .= '/dump_' . $path_suffix . '.spog';
+      if (!file_exists($path)) {
+        $this->saveSPOG($path);
       }
+      readfile($path);
+      exit;
     }
+    echo $this->getHeader();
+    $offset = 0;
+    do {
+      $proceed = 0;
+      $rs = $this->getRecordset($offset);
+      if (!$rs) break;
+      while ($row = mysql_fetch_array($rs)) {
+        echo $this->getEntry($row);
+        $proceed = 1;
+      }
+      $offset += $this->limit;
+    } while ($proceed);
     echo $this->getFooter();
   }
-  
+
   /*  */
-  
+
   function saveSPOG($path, $q = '') {
-    $con = $this->store->getDBCon();
-    $rs = $q ? $this->store->query($q, 'rows') : $this->getRecordset();
-    if ($er = mysql_error($con)) return $this->addError($er);
+    if ($q) return $this->saveCustomSPOG($path, $q);
     if (!$fp = @fopen($path, 'w')) return $this->addError('Could not create backup file at ' . realpath($path));
     fwrite($fp, $this->getHeader());
-    if ($rs) {
-      if ($q) {
-        foreach ($rs as $row) {
-          fwrite($fp, $this->getEntry($row));
-        }
+    $offset = 0;
+    do {
+      $proceed = 0;
+      $rs = $this->getRecordset($offset);
+      if (!$rs) break;
+      while ($row = mysql_fetch_array($rs)) {
+        fwrite($fp, $this->getEntry($row));
+        $proceed = 1;
       }
-      else {
-    		while ($row = mysql_fetch_array($rs)) {
-          fwrite($fp, $this->getEntry($row));
-        }
-      }
+      $offset += $this->limit;
+    } while ($proceed);
+    fwrite($fp, $this->getFooter());
+    @fclose($fp);
+    return 1;
+  }
+
+  /*  */
+
+  function saveCustomSPOG($path, $q) {
+    if (!$fp = @fopen($path, 'w')) return $this->addError('Could not create backup file at ' . realpath($path));
+    fwrite($fp, $this->getHeader());
+    $rows = $this->store->query($q, 'rows');
+    foreach ($rows as $row) {
+      fwrite($fp, $this->getEntry($row));
     }
     fwrite($fp, $this->getFooter());
     @fclose($fp);
@@ -68,7 +95,7 @@ class ARC2_StoreDumper extends ARC2_Class {
   
   /*  */
 
-  function getRecordset() {
+  function getRecordset($offset) {
     $prefix = $this->store->getTablePrefix();
     $con = $this->store->getDBCon();
     $sql = '
@@ -91,7 +118,13 @@ class ARC2_StoreDumper extends ARC2_Class {
         JOIN ' . $prefix . 'g2t G2T ON (T.t = G2T.t)
         JOIN ' . $prefix . 'id2val VG ON (G2T.g = VG.id)
     ';
-    return mysql_unbuffered_query($sql, $con);
+    if ($this->limit) $sql .= ' LIMIT ' . $this->limit;
+    if ($offset) $sql .= ' OFFSET ' . $offset;
+    $rs = mysql_unbuffered_query($sql, $con);
+    if (($err = mysql_error($con))) {
+      return $this->addError($err);
+    }
+    return $rs;
   }
 
   /*  */
@@ -112,6 +145,7 @@ class ARC2_StoreDumper extends ARC2_Class {
   }
 
   function getEntry($row) {
+    if (!$this->keep_time_limit) @set_time_limit($this->v('time_limit', 1200, $this->a));
     $n = "\n";
     $r = '';
     $r .= $n . '    <result>';
@@ -119,11 +153,12 @@ class ARC2_StoreDumper extends ARC2_Class {
       if (isset($row[$var])) {
         $type = (string) $row[$var . ' type'];
         $r .= $n . '      <binding name="' . $var . '">';
+        $val = $this->toUTF8($row[$var]);
         if (($type == '0') || ($type == 'uri')) {
-          $r .= $n . '        <uri>' . htmlspecialchars($row[$var]) . '</uri>';
+          $r .= $n . '        <uri>' . $this->getSafeValue($val) . '</uri>';
         }
         elseif (($type == '1') || ($type == 'bnode')) {
-          $r .= $n . '        <bnode>' . substr($row[$var], 2) . '</bnode>';
+          $r .= $n . '        <bnode>' . substr($val, 2) . '</bnode>';
         }
         else {
           $lang_dt = '';
@@ -134,13 +169,47 @@ class ARC2_StoreDumper extends ARC2_Class {
           list($lang, $dt) = $is_lang ? array($lang_dt, '') : array('', $lang_dt);
           $lang = $lang ? ' xml:lang="' . $lang . '"' : '';
           $dt = $dt ? ' datatype="' . htmlspecialchars($dt) . '"' : '';
-          $r .= $n . '        <literal' . $dt . $lang . '>' . htmlspecialchars($row[$var]) . '</literal>';
+          $r .= $n . '        <literal' . $dt . $lang . '>' . $this->getSafeValue($val) . '</literal>';
         }
         $r .= $n . '      </binding>';
       }
     }
     $r .= $n . '    </result>';
     return $r;
+  }
+
+  function getSafeValue($val) {/* mainly for fixing json_decode bugs */
+    $mappings = array(
+      '%00' => '',
+      '%01' => '',
+      '%02' => '',
+      '%03' => '',
+      '%04' => '',
+      '%05' => '',
+      '%06' => '',
+      '%07' => '',
+      '%08' => '',
+      '%09' => '',
+      '%0B' => '',
+      '%0C' => '',
+      '%0E' => '',
+      '%0F' => '',
+      '%15' => '',
+      '%17' => 'ė',
+      '%1A' => ',',
+      '%1F' => '',
+    );
+    $froms = array_keys($mappings);
+    $tos = array_values($mappings);
+    foreach ($froms as $i => $from) $froms[$i] = urldecode($from);
+    $val = str_replace($froms, $tos, $val);
+    if (strpos($val, '</') !== false) {
+      $val = "\n<![CDATA[\n" . $val . "\n]]>\n";
+    }
+    else {
+      $val = htmlspecialchars($val);
+    }
+    return $val;
   }
 
   function getFooter() {
