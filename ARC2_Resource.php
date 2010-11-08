3a59905fd5faf70505a18a147ba21dbb05e88389
semsol / Benjamin Nowack <bnowack@semsol.com>
2010-11-08 09:30:41 +0100

    Switching from DESCRIBE to CONSTRUCT for more predictable results when working with different SPARQL endpoints. (Thanks to Miles Worthington)

--- ARC2_Resource.php
+++ ARC2_Resource.php
@@ -6,7 +6,7 @@
  * @license http://arc.semsol.org/license
  * @homepage <http://arc.semsol.org/>
  * @package ARC2
- * @version 2010-02-23
+ * @version 2010-11-08
 */
 
 ARC2::inc('Class');
@@ -65,7 +65,7 @@ class ARC2_Resource extends ARC2_Class {
     if (in_array($uri, $this->fetched)) return 0;
     $this->index[$uri] = array();
     if ($this->store) {
-      $index = $this->store->query('DESCRIBE <' . $uri . '>', 'raw');
+      $index = $this->store->query('CONSTRUCT { <' . $uri . '> ?p ?o . } WHERE { <' . $uri . '> ?p ?o . } ', 'raw');
     }
     else {
       $index = $this->toIndex($uri);
