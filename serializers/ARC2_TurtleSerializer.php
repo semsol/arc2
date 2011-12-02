c7c03dad5d489175fc94ba2aca845fde57cf62b5
Benjamin Nowack <mail@bnowack.de>
2011-12-02 11:02:23 +0100

    Added a URI-fallback to the NTriplesSerializer for non-unicode-enabled environments that try to serialise subjects or predicates containing multi-byte characters.

--- serializers/ARC2_TurtleSerializer.php
+++ serializers/ARC2_TurtleSerializer.php
@@ -39,7 +39,7 @@ class ARC2_TurtleSerializer extends ARC2_RDFSerializer {
       ) {
         return $pn;
       }
-      if (preg_match('/^[a-z0-9]+\:[^\s]*$/is', $v)) {
+      if (preg_match('/^[a-z0-9]+\:[^\s]*$/is' . ($this->has_pcre_unicode ? 'u' : ''), $v)) {
         return '<' .$v. '>';
       }
       return $this->getTerm(array('type' => 'literal', 'value' => $v), $term, $qualifier);
