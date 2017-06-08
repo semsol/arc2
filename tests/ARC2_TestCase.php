7b8edc7edfb6786bca014d2f604afa798f3f3366
Konrad Abicht <k.abicht@googlemail.com>
2017-06-08 15:33:21 +0200

    setup PHPUnit environment; fixed tests
    * removed warnings like Notice: Only variables should be passed by reference in
    /var/www/html/tests/unit/ARC2_ClassTest.php on line 8
    
    * fixed problem Warning: array_merge(): Argument #1 is not an array in
    /var/www/html/vendor/phpunit/phpunit/src/Framework/TestCase.php on line 1064 for
    every test case

--- tests/ARC2_TestCase.php
+++ tests/ARC2_TestCase.php
@@ -1,13 +1,5 @@
 <?php
 
-define('ARC2_DIR', __DIR__ . '/../');
-
-require_once ARC2_DIR . 'ARC2.php';
-
-class ARC2_TestCase extends PHPUnit_Framework_TestCase {
-
-    public function __construct() {
-		
-    }
-	
+class ARC2_TestCase extends PHPUnit\Framework\TestCase
+{
 }
