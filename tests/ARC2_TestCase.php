f55e6e7d2a5fa7107bf2513185e8fdc0776fb15b
Konrad Abicht <k.abicht@googlemail.com>
2017-12-15 18:02:50 +0100

    test environment: preparations for further changes

--- tests/ARC2_TestCase.php
+++ tests/ARC2_TestCase.php
@@ -4,7 +4,19 @@ namespace Tests;
 
 class ARC2_TestCase extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * Store configuration to connect with the database.
+     *
+     * @var array
+     */
     protected $dbConfig;
+
+    /**
+     * Subject under test.
+     *
+     * @var mixed
+     */
+    protected $fixture;
 
     public function setUp()
     {
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ .'/../vendor/autoload.php';
 
+error_reporting(E_ALL);
+
 require 'ARC2_TestHandler.php';
 
 global $dbConfig;
