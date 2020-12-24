86a4b6c4e9f5fc5e0a005e25244cbf69a1b8a243
Konrad Abicht <hi@inspirito.de>
2020-12-24 12:34:24 +0100

    SQLite related refinements in bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,55 +25,53 @@ if (file_exists(__DIR__.'/config.php')) {
      * Parameter are set from outside using environment variables.
      * Please check YAML files in .github/workflows for details.
      */
-    $mysqlTestConfig = [
-        'db_name' => 'arc2_test',
-        'db_user' => 'root',
-        'db_pwd'  => 'Pass123',
-        'db_host' => '127.0.0.1',
-        'db_port' => $_SERVER['DB_PORT'] ?: 3306,
-    ];
-
-    /**
-     * DB Adapter (PDO or mysqli)
-     */
-    $dbAdapter = getenv('DB_ADAPTER') ?? $_SERVER['DB_ADAPTER'];
-    if ('pdo' == $dbAdapter) {
-        $dbConfig['db_adapter'] = 'pdo';
-        $dbConfig['db_pdo_protocol'] = getenv('DB_PDO_PROTOCOL') ?? $_SERVER['DB_PDO_PROTOCOL'];
-
-        if (empty($dbConfig['db_pdo_protocol'])) {
-            throw new \Exception(
-                'Environment variable DB_PDO_PROTOCOL not set. Possible values are: mysql, sqlite'
-            );
-        }
-    } elseif ('mysqli' == $dbAdapter) {
-        $dbConfig['db_adapter'] = 'mysqli';
-    } else {
-        throw new Exception('Neither environment variable DB_ADAPTER nor $_SERVER["DB_ADAPTER"] are set.');
-    }
-
-    // set defaults for dbConfig entries
-    if (false == isset($dbConfig['store_name'])) {
-        $dbConfig['store_name'] = 'arc';
-    }
-
-    if (false == isset($dbConfig['db_table_prefix'])) {
-        $dbConfig['db_table_prefix'] = null;
-    }
 
     // SQLite in memory only: unset db_name to force it to use sqlite::memory:
-    $useMemory = getenv('DB_USE_MEMORY') ?? $_SERVER['DB_USE_MEMORY'];
-    if ('true' == $useMemory) {
-        unset($dbConfig['db_name']);
-    }
+    $useSQLiteMemory = getenv('DB_SQLITE_IN_MEMORY') ?? $_SERVER['DB_SQLITE_IN_MEMORY'];
+    if ('true' == $useSQLiteMemory) {
+        $dbConfig = ['db_adapter' => 'pdo', 'db_pdo_protocol' => 'sqlite'];
+    } else {
+        $dbConfig = [
+            'db_name' => 'arc2_test',
+            'db_user' => 'root',
+            'db_pwd'  => 'Pass123',
+            'db_host' => '127.0.0.1',
+            'db_port' => $_SERVER['DB_PORT'] ?: 3306,
+        ];
 
-    /*
-    * set cache enable
-    *
-    * if enabled, we use an instance of ArrayCache which is very fast
-    */
-    if (true === getenv('CACHE_ENABLED') || 'true' == getenv('CACHE_ENABLED')) {
-        $dbConfig['cache_enabled'] = true;
-        $dbConfig['cache_instance'] = new Symfony\Component\Cache\Simple\ArrayCache();
+        /**
+         * DB Adapter (PDO or mysqli)
+         */
+        $dbConfig['db_adapter'] = getenv('DB_ADAPTER') ?? $_SERVER['DB_ADAPTER'];
+        if ('pdo' == $dbConfig['db_adapter']) {
+            $dbConfig['db_pdo_protcol'] = getenv('DB_PDO_PROTOCOL') ?? $_SERVER['DB_PDO_PROTOCOL'];
+
+            if (empty($dbConfig['db_pdo_protocol'])) {
+                throw new \Exception(
+                    'Environment variable DB_PDO_PROTOCOL not set. Possible values are: mysql, sqlite'
+                );
+            }
+        } elseif ('mysqli' == $dbAdapter) {
+            $dbConfig['db_adapter'] = 'mysqli';
+        } else {
+            throw new Exception('Neither environment variable DB_ADAPTER nor $_SERVER["DB_ADAPTER"] are set.');
+        }
+
+        // set defaults for dbConfig entries
+        if (false == isset($dbConfig['store_name'])) {
+            $dbConfig['store_name'] = 'arc';
+        }
+
+        $dbConfig['db_table_prefix'] = $dbConfig['db_table_prefix'] ?? null;
+
+        /*
+        * set cache enable
+        *
+        * if enabled, we use an instance of ArrayCache which is very fast
+        */
+        if (true === getenv('CACHE_ENABLED') || 'true' == getenv('CACHE_ENABLED')) {
+            $dbConfig['cache_enabled'] = true;
+            $dbConfig['cache_instance'] = new Symfony\Component\Cache\Simple\ArrayCache();
+        }
     }
 }
