2c48d826e507335b0102b93ce63c98c589cbd0ce
Konrad Abicht <hi@inspirito.de>
2020-12-24 13:20:47 +0100

    tests/bootstrap: fixed outdated variable

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -26,11 +26,16 @@ if (file_exists(__DIR__.'/config.php')) {
      * Please check YAML files in .github/workflows for details.
      */
 
-    // SQLite in memory only: unset db_name to force it to use sqlite::memory:
+    /*
+     * if SQLite in memory only: unset db_name to force it to use sqlite::memory:
+     */
     $useSQLiteMemory = getenv('DB_SQLITE_IN_MEMORY') ?? $_SERVER['DB_SQLITE_IN_MEMORY'];
     if ('true' == $useSQLiteMemory) {
         $dbConfig = ['db_adapter' => 'pdo', 'db_pdo_protocol' => 'sqlite'];
     } else {
+        /**
+         * Either one of: pdo (mysql, sqlite), mysqli
+         */
         $dbConfig = [
             'db_name' => 'arc2_test',
             'db_user' => 'root',
@@ -51,7 +56,7 @@ if (file_exists(__DIR__.'/config.php')) {
                     'Environment variable DB_PDO_PROTOCOL not set. Possible values are: mysql, sqlite'
                 );
             }
-        } elseif ('mysqli' == $dbAdapter) {
+        } elseif ('mysqli' == $dbConfig['db_adapter']) {
             $dbConfig['db_adapter'] = 'mysqli';
         } else {
             throw new Exception('Neither environment variable DB_ADAPTER nor $_SERVER["DB_ADAPTER"] are set.');
@@ -69,7 +74,8 @@ if (file_exists(__DIR__.'/config.php')) {
         *
         * if enabled, we use an instance of ArrayCache which is very fast
         */
-        if (true === getenv('CACHE_ENABLED') || 'true' == getenv('CACHE_ENABLED')) {
+        $cacheEnabled = getenv('CACHE_ENABLED') ?? $_SERVER['CACHE_ENABLED'];
+        if (true === $cacheEnabled || 'true' == $cacheEnabled) {
             $dbConfig['cache_enabled'] = true;
             $dbConfig['cache_instance'] = new Symfony\Component\Cache\Simple\ArrayCache();
         }
